f16e4dc8cc8372e3925941863e80a0fa323f80a5
root <root@localhost.localdomain>
2014-05-02 16:11:43 +0200

    Added to git

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""
+    Setup for Panthera buildZip application
+"""
+ 
+from distutils.core import setup 
+
+setup(
+    name='Panthera zip/targz builder from git',
+    author='Damian Kęska',
+    author_email='damian@darbs.pl',
+    scripts=['panthera-buildZip']
+)
